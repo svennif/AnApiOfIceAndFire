b3f0ea1a964120c72435684148640fa9308cd926
Joakim Skoog <glasvegas@hotmail.se>
2016-04-03 13:27:10 +0200

    Added data to Organisation

--- AnApiOfIceAndFire/Models/v1/Organisation.cs
+++ AnApiOfIceAndFire/Models/v1/Organisation.cs
@@ -1,7 +1,14 @@
-﻿namespace AnApiOfIceAndFire.Models.v1
+﻿using System.Collections.Generic;
+
+namespace AnApiOfIceAndFire.Models.v1
 {
     public class Organisation
     {
-         
+        public string URL { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public string Founded { get; }
+        public string Founder { get; }
+        public IEnumerable<string> KnownMembers { get; } 
     }
 }
