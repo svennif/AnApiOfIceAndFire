479a688037d11ebcf5aa7cc57442fb1abc2b9a3b
Joakim Skoog <glasvegas@hotmail.se>
2016-04-03 13:57:27 +0200

    Added fields to domain organisation

--- AnApiOfIceAndFire.Domain/Models/IOrganisation.cs
+++ AnApiOfIceAndFire.Domain/Models/IOrganisation.cs
@@ -1,7 +1,14 @@
-﻿namespace AnApiOfIceAndFire.Domain.Models
+﻿using System.Collections.Generic;
+
+namespace AnApiOfIceAndFire.Domain.Models
 {
     public interface IOrganisation
     {
-         int Identifier { get; }
+        int Identifier { get; }
+        string Name { get; }
+        string Description { get; }
+        string Founded { get; }
+        ICharacter Founder { get; }
+        IReadOnlyCollection<ICharacter> KnownMembers { get; } 
     }
 }
