6d7cbca126962502cd4aed8d8c03d674b1adb609
Joakim Skoog <glasvegas@hotmail.se>
2016-04-03 13:29:16 +0200

    Fixed constructor and properties

--- AnApiOfIceAndFire/Models/v1/Organisation.cs
+++ AnApiOfIceAndFire/Models/v1/Organisation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AnApiOfIceAndFire.Models.v1
 {
@@ -6,9 +7,17 @@ namespace AnApiOfIceAndFire.Models.v1
     {
         public string URL { get; }
         public string Name { get; }
-        public string Description { get; }
-        public string Founded { get; }
-        public string Founder { get; }
-        public IEnumerable<string> KnownMembers { get; } 
+        public string Description { get; set; }
+        public string Founded { get; set; }
+        public string Founder { get; set; }
+        public IEnumerable<string> KnownMembers { get; set; }
+
+        public Organisation(string url, string name)
+        {
+            if (url == null) throw new ArgumentNullException(nameof(url));
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            URL = url;
+            Name = name;
+        }
     }
 }
