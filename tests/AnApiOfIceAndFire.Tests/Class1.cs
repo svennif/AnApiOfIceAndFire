363e22d5563a4838e8cbbb1426bd2a88ba95d693
joakimskoog <joakimskooog@gmail.com>
2016-12-27 15:10:21 +0100

    Replaced old test project with xunit

---
+++ tests/AnApiOfIceAndFire.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AnApiOfIceAndFire.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void ThisTestShouldPass()
+        {
+            //We use this dummy test to test if the CI-environment runs the tests properly. Remove this after verifying that stuff works
+            Assert.Equal(2, (1+1));
+        }
+    }
+}
