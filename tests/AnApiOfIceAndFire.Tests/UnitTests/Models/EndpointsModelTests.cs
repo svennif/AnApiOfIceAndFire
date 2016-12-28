f660498de5a5a8bfe52fc3b77788c3f9c5d5fab8
joakimskoog <joakimskooog@gmail.com>
2016-12-28 20:23:00 +0100

    Added tests for endpoints constructor

---
+++ tests/AnApiOfIceAndFire.Tests/UnitTests/Models/EndpointsModelTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AnApiOfIceAndFire.Models;
+using Xunit;
+
+namespace AnApiOfIceAndFire.Tests.UnitTests.Models
+{
+    public class EndpointsModelTests
+    {
+        [Fact]
+        public void GivenEmptyBooksUrl_WhenCreatingEndpoints_ThenArgumentExceptionIsThrown()
+        {
+            Assert.Throws<ArgumentException>(() => new EndpointsModel(string.Empty, "http://localhost:55686/api/characters", "http://localhost:55686/api/houses"));
+        }
+
+        [Fact]
+        public void GivenEmptyCharactersUrl_WhenCreatingEndpoints_ThenArgumentExceptionIsThrown()
+        {
+            Assert.Throws<ArgumentException>(() => new EndpointsModel("http://localhost:55686/api/books", string.Empty, "http://localhost:55686/api/houses"));
+        }
+
+        [Fact]
+        public void GivenEmptyHousesUrl_WhenCreatingEndpoints_ThenArgumentExceptionIsThrown()
+        {
+            Assert.Throws<ArgumentException>(() => new EndpointsModel("http://localhost:55686/api/books", "http://localhost:55686/api/characters", string.Empty));
+        }
+    }
+}
